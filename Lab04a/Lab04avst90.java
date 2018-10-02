65e1217ff82796aa65e9c6d416ffda6e0333a3c3
joec19 <joe.c19@student.parishepiscopal.org>
2018-10-02 15:08:40 -0500

    Revised Submission

---
+++ Lab04a/Lab04avst90.java
@@ -0,0 +1,31 @@
+// Lab04avst.java
+// The Mortgage Payment Program
+// This the student, starting version of the Lab04a assignment.
+
+import java.lang.Math;
+
+public class Lab04avst90
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Lab04a, Student Version\n");
+
+        double principal      = 250000;
+        double annualRate     = 4.85;
+        double numYears       = 30;
+        double monthlyPayment = ((principal * (annualRate / 12) * Math.pow(
+        1 + (annualRate / 100) / 12, numYears * 12)) / (Math.pow(1 + 
+        (annualRate / 100) / 12, numYears * 12) - 1)) / 100;
+        double totalPayment   = monthlyPayment * numYears * 12;
+        double totalInterest  = totalPayment - principal;
+        
+
+        System.out.println("Principal:         $" + principal);
+        System.out.println("Annual Rate:       " + annualRate + "%");
+        System.out.println("Number of Years:   " + numYears);
+        System.out.println("Monthly Payment:   $" + monthlyPayment);
+        System.out.println("Total Payments:    $" + totalPayment);
+        System.out.println("Total Interst:     $" + totalInterest);
+    }
+}
+
